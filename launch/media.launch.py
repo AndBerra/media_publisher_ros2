b441d56b987b9b1455620dfea8a19fca5dba290e
AndBerra <andrea.berra@outlook.com>
2025-08-04 21:49:02 +0200

    added option to enable or not record of rosbag

--- launch/media.launch.py
+++ launch/media.launch.py
@@ -1,8 +1,14 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    DeclareLaunchArgument,
+    ExecuteProcess,
+)
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
@@ -12,6 +18,26 @@ def generate_launch_description():
 
     # Get the package's share directory
     media_publisher_pkg_dir = get_package_share_directory("media_publisher")
+
+    record_bag_arg = DeclareLaunchArgument(
+        "record_bag",
+        default_value="false",
+        description="Set to 'true' to record all topics to a ROS2 bag file.",
+    )
+
+    # get rosbag as param
+    record_bag_arg = DeclareLaunchArgument(
+        "record_bag",
+        default_value="false",
+        description="Set to 'true' to record all topics to a ROS2 bag file.",
+    )
+
+    # This action will only be executed if the 'record_bag' launch argument is 'true'.
+    bag_recorder_process = ExecuteProcess(
+        condition=IfCondition(LaunchConfiguration("record_bag")),
+        cmd=["ros2", "bag", "record", "-a"],
+        output="screen",
+    )
 
     # Create the IncludeLaunchDescription action for the audio launch file
     audio_launch_file = IncludeLaunchDescription(
@@ -29,7 +55,9 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            record_bag_arg,
             audio_launch_file,
             video_launch_file,
+            bag_recorder_process,
         ]
     )
