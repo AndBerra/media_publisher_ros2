7f3ec0348981a0b4d47474f5964eed62109ee72a
AndBerra <andrea.berra@outlook.com>
2025-08-05 22:23:46 +0200

    specified folder to store ros2bags and file name format. The folder is added as volume to have file on my pc

--- launch/media.launch.py
+++ launch/media.launch.py
@@ -9,6 +9,7 @@ from launch.actions import (
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from datetime import datetime
 
 
 def generate_launch_description():
@@ -32,10 +33,18 @@ def generate_launch_description():
         description="Set to 'true' to record all topics to a ROS2 bag file.",
     )
 
+    # define storage folder for bags
+    storage_folder = "/ros2_bags"
+    # define file name
+    bag_filename = "rosbag_" + datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+
+    # get path to store file
+    output_path = os.path.join(storage_folder, bag_filename)
+
     # This action will only be executed if the 'record_bag' launch argument is 'true'.
     bag_recorder_process = ExecuteProcess(
         condition=IfCondition(LaunchConfiguration("record_bag")),
-        cmd=["ros2", "bag", "record", "-a"],
+        cmd=["ros2", "bag", "record", "-a", "-o", output_path],
         output="screen",
     )
 
